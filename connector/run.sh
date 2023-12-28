75b150732725f45e4870830b89776f47a8104c95
Renjith <admin@personal-mac.lan>
2023-12-27 20:39:50 -0800

    fix : install script

--- connector/run.sh
+++ connector/run.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-source $(dirname "$0")/env 2> /dev/null
-if [ $? -ne 0 ];then
-    . $(dirname "$0")/env # For mac environments
-fi
-export PYTHONPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/src"
-gunicorn --bind ${BIND_ADDRESS:="127.0.0.1:9081"} wsgi:app
+
+set_pythonpath() {
+    local script_dir
+    script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+    export PYTHONPATH="$script_dir/src"
+}
+
+load_environment() {
+    source "$(dirname "$0")/env" 2> /dev/null || source "$(dirname "$0")/env" # For macOS environments
+}
+
+start_server() {
+    local bind_address="${BIND_ADDRESS:=127.0.0.1:9081}"
+    gunicorn --bind "$bind_address" wsgi:app
+}
+
+main() {
+    load_environment
+    set_pythonpath
+    start_server
+}
+
+main
